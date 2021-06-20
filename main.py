e02e2d4dd383ddb3f1e88ab7395b869302ab4fac
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-06-20 16:54:57 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+# ------------------------------------------
+os.system('git add .')
+os.system('git commit -m "Initial Commit"')
+os.system('git push')
+# ------------------------------------------
+

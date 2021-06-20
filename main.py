751cf7ed0fd54213fde3fdb2d60ac6ddbf4c3460
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-06-20 17:02:47 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ os.system('git push')
 WIDTH = 750
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 
+def redraw_window(win):
+    win.fill((255, 255, 255))
+
 def main(win, width):
     run = True
 
     while run:
+        redraw_window(win)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False

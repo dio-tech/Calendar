90132eaaad427ac0740161896fa83c3e96c76038
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-06-20 17:03:06 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def main(win, width):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        pygame.display.update()
 
 main(WIN, WIDTH)
 

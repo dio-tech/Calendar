da25b317bd16c66a16aa8f544a788ed4a4aef91a
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-06-20 17:01:30 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import os
+import pygame
 
 # ------------------------------------------
 os.system('git add .')
 os.system('git commit -m "Initial Commit"')
 os.system('git push')
 # ------------------------------------------
+
+WIDTH = 750, 750
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+
+def main(win, width):
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main(WIN, WIDTH)
 
